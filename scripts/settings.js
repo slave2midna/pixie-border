818d07b2f808f23ab64b3c7bc7ca8048e109cea5
Noamancy <69311684+slave2midna@users.noreply.github.com>
2025-09-18 16:18:39 -0500

    reordered settings

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,32 +1,7 @@
 const MODULE_ID = "pixie-border";
 
 Hooks.once("init", () => {
-  // Color mode
-  game.settings.register(MODULE_ID, "mode", {
-    name: game.i18n.localize("pixie-border.settings.mode.name"),
-    hint: game.i18n.localize("pixie-border.settings.mode.hint"),
-    scope: "world",
-    config: true,
-    type: String,
-    default: "disposition",
-    choices: {
-      disposition: game.i18n.localize("pixie-border.settings.mode.choices.disposition"),
-      custom: game.i18n.localize("pixie-border.settings.mode.choices.custom"),
-      condition: game.i18n.localize("pixie-border.settings.mode.choices.condition")
-    }
-  });
-
-  // Custom color (used only when mode=custom)
-  game.settings.register(MODULE_ID, "customColor", {
-    name: game.i18n.localize("pixie-border.settings.customColor.name"),
-    hint: game.i18n.localize("pixie-border.settings.customColor.hint"),
-    scope: "world",
-    config: true,
-    type: String,
-    default: "#88ccff"
-  });
-
-  // Visual thickness (pixels)
+  // Border Thickness
   game.settings.register(MODULE_ID, "thickness", {
     name: game.i18n.localize("pixie-border.settings.thickness.name"),
     hint: game.i18n.localize("pixie-border.settings.thickness.hint"),
@@ -37,7 +12,7 @@ Hooks.once("init", () => {
     range: { min: 1, max: 5, step: 1 }
   });
 
-  // Hide Foundry's default square token border
+  // Disable Foundry Border
   game.settings.register(MODULE_ID, "hideDefaultBorder", {
     name: game.i18n.localize("pixie-border.settings.hideDefaultBorder.name"),
     hint: game.i18n.localize("pixie-border.settings.hideDefaultBorder.hint"),
@@ -45,20 +20,20 @@ Hooks.once("init", () => {
     config: true,
     type: Boolean,
     default: false,
-    requiresReload: true 
+    requiresReload: true
   });
 
-  // Enable target border
+  // Enable Target Border
   game.settings.register(MODULE_ID, "enableTarget", {
     name: game.i18n.localize("pixie-border.settings.enableTarget.name"),
     hint: game.i18n.localize("pixie-border.settings.enableTarget.hint"),
-    scope: "world",
+    scope: "client",
     config: true,
     type: Boolean,
     default: false
   });
 
-  // Enable border glow
+  // Enable Border Glow
   game.settings.register(MODULE_ID, "enableGlow", {
     name: game.i18n.localize("pixie-border.settings.enableGlow.name"),
     hint: game.i18n.localize("pixie-border.settings.enableGlow.hint"),
@@ -68,7 +43,7 @@ Hooks.once("init", () => {
     default: false
   });
 
-  // Glow distance (pixels)
+  // Glow Distance
   game.settings.register(MODULE_ID, "glowDistance", {
     name: game.i18n.localize("pixie-border.settings.glowDistance.name"),
     hint: game.i18n.localize("pixie-border.settings.glowDistance.hint"),
@@ -79,7 +54,7 @@ Hooks.once("init", () => {
     range: { min: 1, max: 64, step: 1 }
   });
 
-  // Glow strength
+  // Glow Strength
   game.settings.register(MODULE_ID, "glowOuterStrength", {
     name: game.i18n.localize("pixie-border.settings.glowOuterStrength.name"),
     hint: game.i18n.localize("pixie-border.settings.glowOuterStrength.hint"),
@@ -88,5 +63,42 @@ Hooks.once("init", () => {
     type: Number,
     default: 4,
     range: { min: 0, max: 10, step: 0.5 }
+  });
+
+  // Color Mode
+  const modeDisp = game.i18n.localize("pixie-border.settings.mode.choices.disposition");
+  const modeCust = game.i18n.localize("pixie-border.settings.mode.choices.custom");
+  const modeCond = game.i18n.localize("pixie-border.settings.mode.choices.condition");
+  const modeHint = `
+<div style="margin-top:0.25rem">
+  <div style="font-weight:600">${game.i18n.localize("pixie-border.settings.mode.hint")}</div>
+  <ul style="margin:0.25rem 0 0.5rem 1.25rem; list-style: disc;">
+    <li><b>${modeDisp}:</b> ${game.i18n.localize("pixie-border.mode.help.disposition")}</li>
+    <li><b>${modeCust}:</b> ${game.i18n.localize("pixie-border.mode.help.custom")}</li>
+    <li><b>${modeCond}:</b> ${game.i18n.localize("pixie-border.mode.help.condition")}</li>
+  </ul>
+</div>`.trim();
+
+  game.settings.register(MODULE_ID, "mode", {
+    name: game.i18n.localize("pixie-border.settings.mode.name"),
+    hint: modeHint,
+    scope: "world",
+    config: true,
+    type: String,
+    default: "disposition",
+    choices: {
+      disposition: modeDisp,
+      custom: modeCust,
+      condition: modeCond
+    }
+  });
+
+  // Custom color
+  game.settings.register(MODULE_ID, "customColor", {
+    name: game.i18n.localize("pixie-border.settings.customColor.name"),
+    hint: game.i18n.localize("pixie-border.settings.customColor.hint"),
+    scope: "world",
+    config: true,
+    type: new foundry.data.fields.ColorField({ initial: "#88ccff" })
   });
 });
