cda770db9f26b13a13d8f84efe6e3e755d6a4a36
Noamancy <69311684+slave2midna@users.noreply.github.com>
2025-09-19 08:29:01 -0500

    inline html

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,7 +1,32 @@
 const MODULE_ID = "pixie-border";
 
 Hooks.once("init", () => {
-  // Border Thickness
+  // Color mode
+  game.settings.register(MODULE_ID, "mode", {
+    name: game.i18n.localize("pixie-border.settings.mode.name"),
+    hint: game.i18n.localize("pixie-border.settings.mode.hint"),
+    scope: "world",
+    config: true,
+    type: String,
+    default: "disposition",
+    choices: {
+      disposition: game.i18n.localize("pixie-border.settings.mode.choices.disposition"),
+      custom: game.i18n.localize("pixie-border.settings.mode.choices.custom"),
+      condition: game.i18n.localize("pixie-border.settings.mode.choices.condition")
+    }
+  });
+
+  // Custom color (used only when mode=custom)
+  game.settings.register(MODULE_ID, "customColor", {
+    name: game.i18n.localize("pixie-border.settings.customColor.name"),
+    hint: game.i18n.localize("pixie-border.settings.customColor.hint"),
+    scope: "world",
+    config: true,
+    type: String,
+    default: "#88ccff"
+  });
+
+  // Visual thickness (pixels)
   game.settings.register(MODULE_ID, "thickness", {
     name: game.i18n.localize("pixie-border.settings.thickness.name"),
     hint: game.i18n.localize("pixie-border.settings.thickness.hint"),
@@ -12,7 +37,7 @@ Hooks.once("init", () => {
     range: { min: 1, max: 5, step: 1 }
   });
 
-  // Disable Foundry Border
+  // Hide Foundry's default square token border
   game.settings.register(MODULE_ID, "hideDefaultBorder", {
     name: game.i18n.localize("pixie-border.settings.hideDefaultBorder.name"),
     hint: game.i18n.localize("pixie-border.settings.hideDefaultBorder.hint"),
@@ -20,20 +45,20 @@ Hooks.once("init", () => {
     config: true,
     type: Boolean,
     default: false,
-    requiresReload: true
+    requiresReload: true 
   });
 
-  // Enable Target Border (per-user)
+  // Enable target border
   game.settings.register(MODULE_ID, "enableTarget", {
     name: game.i18n.localize("pixie-border.settings.enableTarget.name"),
     hint: game.i18n.localize("pixie-border.settings.enableTarget.hint"),
-    scope: "client",
+    scope: "world",
     config: true,
     type: Boolean,
     default: false
   });
 
-  // Enable Border Glow
+  // Enable border glow
   game.settings.register(MODULE_ID, "enableGlow", {
     name: game.i18n.localize("pixie-border.settings.enableGlow.name"),
     hint: game.i18n.localize("pixie-border.settings.enableGlow.hint"),
@@ -43,7 +68,7 @@ Hooks.once("init", () => {
     default: false
   });
 
-  // Glow Distance
+  // Glow distance (pixels)
   game.settings.register(MODULE_ID, "glowDistance", {
     name: game.i18n.localize("pixie-border.settings.glowDistance.name"),
     hint: game.i18n.localize("pixie-border.settings.glowDistance.hint"),
@@ -54,7 +79,7 @@ Hooks.once("init", () => {
     range: { min: 1, max: 64, step: 1 }
   });
 
-  // Glow Strength
+  // Glow strength
   game.settings.register(MODULE_ID, "glowOuterStrength", {
     name: game.i18n.localize("pixie-border.settings.glowOuterStrength.name"),
     hint: game.i18n.localize("pixie-border.settings.glowOuterStrength.hint"),
@@ -63,25 +88,5 @@ Hooks.once("init", () => {
     type: Number,
     default: 4,
     range: { min: 0, max: 10, step: 0.5 }
-  });
-
-  // Color Mode
-  game.settings.register(MODULE_ID, "mode", {
-    name: game.i18n.localize("pixie-border.settings.mode.name"),
-    hint: modeHint,
-    scope: "world",
-    config: true,
-    type: String,
-    default: "disposition",
-    choices: { disposition: modeDisp, custom: modeCust, condition: modeCond }
-  });
-
-  // Custom color
-  game.settings.register(MODULE_ID, "customColor", {
-    name: game.i18n.localize("pixie-border.settings.customColor.name"),
-    hint: game.i18n.localize("pixie-border.settings.customColor.hint"),
-    scope: "world",
-    config: true,
-    type: new foundry.data.fields.ColorField({ initial: "#88ccff" })
   });
 });
